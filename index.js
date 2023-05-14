346608da7c407252058d26cef8f72aff770d9892
Victor Valenzuela <vikotor@gmail.com>
2023-05-14 09:11:31 -0400

    modificacion index

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const args = process.argv.slice(2);
 
 CrearArchivo();
 if(args[0] == "registrar")
+{
     Registrar(args[1],args[2],args[3],args[4],args[5]);
+    console.log("Cita resistrada");
+}
+    
 else if(args[0] == "leer")
     Leer();
-else
+else if(args[0] == "crear")
     CrearArchivo();
+else
+    console.log("Comando '" + args[0]  + "' no encontrado");
